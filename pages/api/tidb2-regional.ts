a84789a19d2c3ce538c5c13ce547b4bf850b0e7f
shiyuhang <1136742008@qq.com>
2023-09-08 14:19:45 +0800

    fix serverless function

--- pages/api/tidb2-regional.ts
+++ pages/api/tidb2-regional.ts
@@ -1,10 +1,11 @@
-import { NextRequest as Request, NextResponse as Response } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { connect } from '@tidbcloud/serverless';
 
 const start = Date.now();
 
-export default async function api(req: Request, ctx: any) {
-  const count = toNumber(new URL(req.url).searchParams.get("count"));
+export default async function api( request: NextApiRequest,
+                                   response: NextApiResponse) {
+  const count = Number(request.query.count);
   const time = Date.now();
 
   const conn = connect({url: process.env.TiDB_DATABASE_URL})
@@ -17,13 +18,12 @@ export default async function api(req: Request, ctx: any) {
       LIMIT 10`)
   }
 
-  return Response.json({
+
+  return response.json({
     data,
     queryDuration: Date.now() - time,
     invocationIsCold: start === time,
     invocationRegion: (req.headers.get("x-vercel-id") ?? "").split(":")[1] || null,
-  }, {
-    headers: { "x-edge-is-cold": start === time ? "1" : "0" },
   });
 }
 
